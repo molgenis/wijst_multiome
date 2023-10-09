a520c1db94e9ab347665546079913640dbd0f2d0
Roy Oelen <roy.oelen@gmail.com>
2023-10-09 09:13:26 +0200

    - fix merge

--- demultiplexing/mo_create_demuxlet_jobs.sh
+++ demultiplexing/mo_create_demuxlet_jobs.sh
@@ -10,10 +10,10 @@
 #directory and file listings
 LANES_DIR="/groups/umcg-franke-scrna/tmp02/projects/multiome/processed/joint/alignment/b38/"
 LANE_BARCODE_APPEND="outs/filtered_feature_bc_matrix/barcodes.tsv.gz"
-BAM_DIR="/groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/demultiplexing/popscle_tools_filtered/alignment/filtered_gex_alignment/"
+BAM_DIR="/groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/demultiplexing/popscle_tools_filtered/alignment/filtered_alignment/"
 INDIVIDUAL_GENOTYPES="/groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/genotype/genotype_per_lane/"
-OUTPUT_DIR="/groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/demultiplexing/demuxlet/demuxlet_output/gex/"
-JOB_DIR="/groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/demultiplexing/demuxlet/jobs/gex/"
+OUTPUT_DIR="/groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/demultiplexing/demuxlet/demuxlet_output/"
+JOB_DIR="/groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/demultiplexing/demuxlet/jobs/"
 DEMUXLET_CMD="singularity exec --bind /groups/umcg-franke-scrna/tmp02/ /groups/umcg-franke-scrna/tmp02/software/sc-eqtlgen-consortium-pipeline/wg1/wg1-pipeline-20230308_2.simg popscle demuxlet"
 
 #parameters used
@@ -24,7 +24,7 @@ FIELD="GT"
 for dir in "$LANES_DIR"/*lane*/ ; do
 
     lane_id=$(basename $dir)
-    output_job=${JOB_DIR}"/demux_gex_"${lane_id}"_SBATCH.sh"
+    output_job=${JOB_DIR}"/demux_"${lane_id}"_SBATCH.sh"
     bam_loc=${BAM_DIR}/"mo_"${lane_id}"_genofiltered_gex_possorted_bam.bam"
 
     echo -e "#!/usr/bin/env bash
@@ -44,7 +44,7 @@ for dir in "$LANES_DIR"/*lane*/ ; do
                 --tag-group ${TAG_GROUP} \\
                 --tag-UMI ${TAG_UMI} \\
                 --field ${FIELD} \\
-                --vcf ${INDIVIDUAL_GENOTYPES}mo_gex_bamsorted_${lane_id}_maf005.vcf.gz \\
+                --vcf ${INDIVIDUAL_GENOTYPES}mo_bamsorted_${lane_id}_maf005.vcf.gz \\
                 --out ${OUTPUT_DIR}${lane_id} \\
                 --group-list ${LANES_DIR}${lane_id}/${LANE_BARCODE_APPEND}
         " > ${output_job}
