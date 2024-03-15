e450918c909fe9682dd94e14038bf3dfbd7bd803
Roy Oelen <r.oelen@umcg.nl>
2024-03-15 17:13:17 +0100

    - using rounded fragments for cpeaks

--- cpeaks_preprocess_samples/mo_cpeaks_to_signac.R
+++ cpeaks_preprocess_samples/mo_cpeaks_to_signac.R
@@ -16,6 +16,8 @@ library(Signac)
 library(EnsDb.Hsapiens.v86)
 # for plots
 library(ggplot2)
+# for upset plot of peak sharing
+library(UpSetR)
 
 
 ####################
@@ -23,7 +25,7 @@ library(ggplot2)
 ####################
 
 
-read_cpeaks <- function(cpeaks_loc, cellranger_loc, lane, annotations) {
+read_cpeaks <- function(cpeaks_loc, cellranger_loc, lane, annotations, atac_fragments_append='/outs/atac_fragments.tsv.gz') {
   # read the features
   features <- read.table(paste(cpeaks_loc, '/', lane, '/features.tsv.gz', sep = ''), sep = '\t', header = F)
   features[['peak']] <- paste(features[[1]], ':',  features[[2]], '-', features[[3]], sep = '')
@@ -48,7 +50,7 @@ read_cpeaks <- function(cpeaks_loc, cellranger_loc, lane, annotations) {
   # change to a barcode unique across lanes
   #colnames(peaks) <- metadata$barcode_lane
   # get the fragments
-  fragments_loc <- paste(cellranger_loc, '/', lane, '/outs/atac_fragments.tsv.gz', sep = '')
+  fragments_loc <- paste(cellranger_loc, '/', lane, atac_fragments_append, sep = '')
   # create the chromatin assay
   chrom_assay <- CreateChromatinAssay(
     counts = peaks,
@@ -223,8 +225,10 @@ get_color_coding_dict <- function(){
   color_coding[["plasmablast"]] <- "#DB8E00"
   color_coding[["other T"]] <- "#FF63B6"
   color_coding[["T_other"]] <- "#FF63B6"
+  color_coding[["T-other"]] <- "#FF63B6"
   color_coding[["hemapoietic_stem"]] <- "#8B8000"
   color_coding[["hemapoietic stem"]] <- "#8B8000"
+  color_coding[["hemapoietic-stem"]] <- "#8B8000"
   return(color_coding)
 }
 
@@ -247,7 +251,9 @@ label_dict <- function(){
   label_dict[["plasmablast"]] <- "plasmablast"
   label_dict[["platelet"]] <- "platelet"
   label_dict[["T_other"]] <- "other T"
+  label_dict[["T-other"]] <- "other T"
   label_dict[["hemapoietic_stem"]] <- "hemapoietic stem"
+  label_dict[["hemapoietic-stem"]] <- "hemapoietic stem"
   # minor cell types
   label_dict[["CD4_TCM"]] <- "CD4 TCM"
   label_dict[["Treg"]] <- "T regulatory"
@@ -279,6 +285,150 @@ label_dict <- function(){
 }
 
 
+peaks_to_bed <- function(exp_per_group, output_prepend='mo_peaks_', output_append='.bed', min_log10_peak=0) {
+  # check each of the columns
+  for (identity in colnames(exp_per_group)) {
+    # subset to that identity
+    peaks_ident <- exp_per_group[, c(identity), drop = F]
+    # get the locations
+    positions <- data.frame(do.call('rbind', strsplit(rownames(peaks_ident), '-')))
+    # set the colnames properly to bed format
+    colnames(positions) <- c('#chrom', 'start', 'end')
+    # add the rownames themselves as the name
+    positions[['name']] <- rownames(peaks_ident)
+    # and the counts as score
+    positions[['score']] <- log10(peaks_ident[, c(identity)])
+    # remove empty entries
+    positions <- positions[positions[['score']] >= min_log10_peak, ]
+    # make the start and stop numeric
+    positions[['start']] <- as.numeric(positions[['start']])
+    positions[['end']] <- as.numeric(positions[['end']])
+    # and sort
+    positions <- positions[order(positions[['#chrom']], positions[['start']], positions[['end']]), ]
+    # create the output location
+    output_loc <- paste(output_prepend, identity, output_append, sep = '')
+    # write the result
+    write.table(positions, output_loc, row.names = F, col.names = T, quote = F, sep = '\t')
+  }
+}
+
+get_npeaks_per_threshold <- function(exp_per_group, log10_thresholds=c(0,1,2,3,4)) {
+  # we'll do each identity, and store the result in per ident in a list
+  numbers_per_identity <- list()
+  # check each of the columns
+  for (identity in colnames(exp_per_group)) {
+    # subset to that identity
+    peaks_ident <- exp_per_group[, c(identity), drop = F]
+    # get the locations
+    positions <- data.frame(do.call('rbind', strsplit(rownames(peaks_ident), '-')))
+    # set the colnames properly to bed format
+    colnames(positions) <- c('#chrom', 'start', 'end')
+    # add the rownames themselves as the name
+    positions[['name']] <- rownames(peaks_ident)
+    # and the counts as score
+    positions[['score']] <- log10(peaks_ident[, c(identity)])
+    # init dataframe
+    numbers = data.frame(identity = rep(identity, times = length(log10_thresholds)), 
+                         log10_thresh=log10_thresholds, 
+                         npeaks=rep(NA, times = length(log10_thresholds)))
+    # check each threshold and fill in that dataframe
+    for (thresh_i in 1 : length(log10_thresholds)) {
+      # get that threshold
+      threshold <- log10_thresholds[thresh_i]
+      # check now many peaks are left
+      npeaks <- nrow(positions[positions[['score']] >= threshold, ])
+      # put that into the dataframe
+      numbers[thresh_i, 'npeaks'] <- npeaks
+    }
+    # put the result in the list we created
+    numbers_per_identity[[identity]] <- numbers
+  }
+  # now merge all of the idents together in one dataframe
+  numbers_all <- do.call('rbind', numbers_per_identity)
+  return(numbers_all)
+}
+
+
+get_peaks_per_identity <- function(exp_per_group, threshold=0) {
+  # we'll do each identity, and store the result in per ident in a list
+  peaks_per_identity <- list()
+  # check each of the columns
+  for (identity in colnames(exp_per_group)) {
+    # subset to that identity
+    peaks_ident <- exp_per_group[, c(identity), drop = F]
+    # get the locations
+    positions <- data.frame(do.call('rbind', strsplit(rownames(peaks_ident), '-')))
+    # set the colnames properly to bed format
+    colnames(positions) <- c('#chrom', 'start', 'end')
+    # add the rownames themselves as the name
+    positions[['name']] <- rownames(peaks_ident)
+    # and the counts as score
+    positions[['score']] <- log10(peaks_ident[, c(identity)])
+    # check now many peaks are left
+    peaks <- positions[positions[['score']] >= threshold, 'name']
+    # put the result in the list we created
+    peaks_per_identity[[identity]] <- peaks
+  }
+  return(peaks_per_identity)
+}
+
+
+plot_peak_sharing_per_celltype <- function(peaks_per_ct, use_label_dict=T, use_color_dict=T){
+  # rename cell types if requested
+  if(use_label_dict){
+    names(peaks_per_ct) <- label_dict()[names(peaks_per_ct)]
+  }
+  # we'll need this list for colouring
+  queries <- list()
+  sets.bar.color <- 'black'
+  # do colouring
+  if(use_color_dict){
+    # create df to store the number of each set, so we know how to order
+    nrs_df <- NULL
+    # add the colors for the cell types
+    i <- 1
+    for(cell_type in (names(peaks_per_ct))){
+      # get all peaks except this cell type
+      peaks_except_c <- do.call('c', peaks_per_ct[setdiff(names(peaks_per_ct), cell_type)])
+      # then this cell type
+      peaks_celltype<- peaks_per_ct[[cell_type]]
+      # check if this singlet exists
+      if (length(setdiff(peaks_celltype, peaks_except_c)) > 0) {
+        # add for the singles in the intersection sizes
+        ct_list <- list(
+          query = intersects,
+          params = list(cell_type),
+          color = get_color_coding_dict()[[cell_type]],
+          active = T)
+        queries[[i]] <- ct_list
+        i <- i + 1
+      }
+      else {
+        print('blegh')
+      }
+      
+      # add for the DF to order the set sizes
+      numbers_row <- data.frame(ct=c(cell_type), nr=c(length(peaks_per_ct[[cell_type]])), stringsAsFactors = F)
+      if(is.null(nrs_df)){
+        nrs_df <- numbers_row
+      }
+      else{
+        nrs_df <- rbind(nrs_df, numbers_row)
+      }
+    }
+    # get the order of the sets
+    ordered_cts <- nrs_df[order(nrs_df$nr, decreasing = T), 'ct']
+    # add the colors for the sets
+    sets.bar.color <- unlist(get_color_coding_dict()[ordered_cts])
+  }
+  else {
+    queries = NULL
+  }
+  #upset(fromList(peaks_per_ct), order.by = 'freq', nsets = length(peaks_per_ct), queries = queries, sets.bar.color=sets.bar.color, nintersects = length(peaks_per_ct)*length(peaks_per_ct))
+  upset(fromList(peaks_per_ct), order.by = 'freq', nsets = length(peaks_per_ct), sets.bar.color=sets.bar.color, nintersects = length(peaks_per_ct)*length(peaks_per_ct))
+  #return(peaks_per_ct)
+}
+
 
 ####################
 # Settings         #
@@ -302,29 +452,29 @@ genome(annotations) <- "hg38"
 
 # these are the lanes
 lanes <- c('230105_lane1', '230105_lane2', '230105_lane3', '230105_lane4',
-           '230105_lane5', '230105_lane6', '230105_lane7', '230105_lane8'
-           # '230112_lane1', '230112_lane2', '230112_lane3', '230112_lane4',
-           # '230112_lane5', '230112_lane6', '230112_lane7', '230112_lane8',
-           # '230120_lane1', '230120_lane2', '230120_lane3', '230120_lane4',
-           # '230120_lane5', '230120_lane6', '230120_lane7', '230120_lane8',
-           # '230127_lane1', '230127_lane2', '230127_lane3', '230127_lane4',
-           # '230127_lane5', '230127_lane6', '230127_lane7', '230127_lane8',
-           # '230202_lane1', '230202_lane2', '230202_lane3', '230202_lane4',
-           # '230202_lane5', '230202_lane6', '230202_lane7', '230202_lane8',
-           # '230209_lane1', '230209_lane2', '230209_lane3', '230209_lane4',
-           # '230209_lane5', '230209_lane6', '230209_lane7', '230209_lane8',
-           # '230216_lane1', '230216_lane2', '230216_lane3', '230216_lane4',
-           # '230216_lane5', '230216_lane6', '230216_lane7', '230216_lane8',
-           # '230223_lane1', '230223_lane2', '230223_lane3', '230223_lane4',
-           # '230223_lane5', '230223_lane6', '230223_lane7', '230223_lane8',
-           # '230302_lane1', '230302_lane2', '230302_lane3', '230302_lane4',
-           # '230302_lane5', '230302_lane6', '230302_lane7', '230302_lane8',
-           # '230316_lane1', '230316_lane2', '230316_lane3', '230316_lane4',
-           # '230316_lane5', '230316_lane6', '230316_lane7', '230316_lane8'
+           '230105_lane5', '230105_lane6', '230105_lane7', '230105_lane8',
+           '230112_lane1', '230112_lane2', '230112_lane3', '230112_lane4',
+           '230112_lane5', '230112_lane6', '230112_lane7', '230112_lane8',
+           '230120_lane1', '230120_lane2', '230120_lane3', '230120_lane4',
+           '230120_lane5', '230120_lane6', '230120_lane7', '230120_lane8',
+           '230127_lane1', '230127_lane2', '230127_lane3', '230127_lane4',
+           '230127_lane5', '230127_lane6', '230127_lane7', '230127_lane8',
+           '230202_lane1', '230202_lane2', '230202_lane3', '230202_lane4',
+           '230202_lane5', '230202_lane6', '230202_lane7', '230202_lane8',
+           '230209_lane1', '230209_lane2', '230209_lane3', '230209_lane4',
+           '230209_lane5', '230209_lane6', '230209_lane7', '230209_lane8',
+           '230216_lane1', '230216_lane2', '230216_lane3', '230216_lane4',
+           '230216_lane5', '230216_lane6', '230216_lane7', '230216_lane8',
+           '230223_lane1', '230223_lane2', '230223_lane3', '230223_lane4',
+           '230223_lane5', '230223_lane6', '230223_lane7', '230223_lane8',
+           '230302_lane1', '230302_lane2', '230302_lane3', '230302_lane4',
+           '230302_lane5', '230302_lane6', '230302_lane7', '230302_lane8',
+           '230316_lane1', '230316_lane2', '230316_lane3', '230316_lane4',
+           '230316_lane5', '230316_lane6', '230316_lane7'
 )
 
 # location of the cpeaks objects
-cpeaks_loc <- '/groups/umcg-franke-scrna/tmp03/projects/multiome/ongoing/cpeaks_peak_calling/output/default/'
+cpeaks_loc <- '/groups/umcg-franke-scrna/tmp03/projects/multiome/ongoing/cpeaks_peak_calling/output/rounded/'
 # get cellranger loc
 cellranger_loc <- '/groups/umcg-franke-scrna/tmp03/projects/multiome/processed/joint/alignment/b38/'
 # location of the gene annotations
@@ -333,15 +483,15 @@ gtf_loc <- '/groups/umcg-franke-scrna/tmp03/external_datasets/refdata-cellranger
 # check each lane
 for (lane in lanes) {
   # read the object
-  object_lane <- read_cpeaks(cpeaks_loc, cellranger_loc, lane = lane, annotations = annotations)
+  object_lane <- read_cpeaks(cpeaks_loc, cellranger_loc = '/groups/umcg-franke-scrna/tmp03/projects/multiome/ongoing/rounded_fragments/', lane = lane, annotations = annotations, atac_fragments_append = '_rounded_fragments.tsv.gz')
   # save the result
-  cpeaks_object_loc <- paste(cpeaks_loc, '/', lane, '/', 'mo_cpeaks_', lane, '.rds', sep = '')
+  cpeaks_object_loc <- paste(cpeaks_loc, '/', lane, '/', 'mo_rounded_cpeaks_', lane, '.rds', sep = '')
   saveRDS(object_lane, cpeaks_object_loc)
   rm(object_lane)
 }
 
 # add all together
-mo_all <- merge_cpeaks_objects(cpeaks_loc, cellranger_loc, lanes, annotations)
+mo_all <- merge_cpeaks_objects(cpeaks_loc, cellranger_loc = '/groups/umcg-franke-scrna/tmp03/projects/multiome/ongoing/rounded_fragments/', lanes = lanes, annotations = annotations, cpeak_prepend='mo_rounded_cpeaks_', cpeak_append='.rds', frag_append='_rounded_fragments.tsv.gz')
 # reremoved cells not having enough counts
 mo_all <- mo_all[, mo_all$nFeature_peaks > 200]
 # compute nucleosome signal score per cell
@@ -367,7 +517,7 @@ mo_all$pct_reads_in_peaks <- mo_all$peak_region_fragments / mo_all$atac_fragment
 mo_all$blacklist_ratio <- mo_all$blacklist_fraction / mo_all$peak_region_fragments
 mo_all$nucleosome_group <- ifelse(mo_all$nucleosome_signal > 4, 'NS > 4', 'NS < 4')
 # save intermediate result
-saveRDS(mo_all, '/groups/umcg-franke-scrna/tmp03/projects/multiome/ongoing/cpeaks_peak_calling/output/default/mo_cpeaks_unfiltered_230105_lane1-8.rds')
+saveRDS(mo_all, '/groups/umcg-franke-scrna/tmp03/projects/multiome/ongoing/cpeaks_peak_calling/output/default/mo_cpeaks_unfiltered.rds')
 # remove outliers
 ncol(mo_all)
 # [1] 96919
@@ -430,3 +580,57 @@ mo_all <- NormalizeData(
   scale.factor = median(mo_all$nCount_activity)
 )
 saveRDS(mo_all, '/groups/umcg-franke-scrna/tmp03/projects/multiome/ongoing/cpeaks_peak_calling/output/default/mo_cpeaks_230105_lane1-8.rds')
+
+# use multi-ome martijn FRIP
+mo_all <- mo_all[, mo_all@meta.data$pct_reads_in_peaks >= 50]
+ncol(mo_all)
+# [1] 60173
+
+# # check only fold changes first
+# Idents(mo_all) <- 'cell_type_final_lowerres'
+# da_folds_per_ct <- list()
+# for (cell_type in unique(mo_all@meta.data[['cell_type_final_lowerres']])) {
+#   print(cell_type)
+#   da_folds <- FoldChange(mo_all, ident.1 = cell_type)
+#   da_folds <- cbind(data.frame(cell_type = rep(cell_type, times = nrow(da_folds)), feature = rownames(da_folds)), da_folds)
+#   da_folds_per_ct[[cell_type]] <- da_folds
+# }
+# da_folds_all_ct <- do.call('rbind', da_folds_per_ct)
+# da_folds_all_ct_closestfeatures <- ClosestFeature(mo_all, regions = da_folds_all_ct[['feature']])
+# da_folds_all_ct[['closest_gene']] <- da_folds_all_ct_closestfeatures[match(da_folds_all_ct[['feature']], da_folds_all_ct_closestfeatures[['query_region']]), 'gene_id']
+# da_folds_all_ct_03 <- da_folds_all_ct[abs(da_folds_all_ct$avg_log2FC) > 0.3, ]
+# da_folds_all_ct_03 <- da_folds_all_ct_03[order(abs(da_folds_all_ct_03$avg_log2FC), decreasing = T), ]
+# # get the average expression as well
+# avg_peaks <- AverageExpression(mo_all)
+# exp_as_bulk <- AggregateExpression(mo_all, group.by = 'orig.ident')
+# sum_peaks <- AggregateExpression(mo_all, group.by = 'cell_type_final_lowerres')
+# # write these files
+# peaks_to_bed(exp_as_bulk$peaks, output_prepend = '/groups/umcg-franke-scrna/tmp03/projects/multiome/ongoing/cpeaks_peak_calling/compare_other_studies/mo_', output_append = '_aggregate_peaks.bed', min_log10_peak = log10(1))
+# peaks_to_bed(sum_peaks$peaks, output_prepend = '/groups/umcg-franke-scrna/tmp03/projects/multiome/ongoing/cpeaks_peak_calling/compare_other_studies/mo_', output_append = '_aggregate_peaks.bed')
+# # now also check how many peaks we have left, if we do some thresholding
+# sum_peaks_nrs_by_threshold <- get_npeaks_per_threshold(sum_peaks$peaks)
+# ggplot(data = sum_peaks_nrs_by_threshold, mapping = aes(x = log10_thresh, y = npeaks, fill = identity)) + 
+#   geom_bar(stat = 'identity', position = 'dodge') + 
+#   theme(panel.border = element_rect(color="black", fill=NA, size=1.1), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), strip.background = element_rect(colour="white", fill="white")) +
+#   scale_fill_manual(values = get_color_coding_dict()) +
+#   xlab('log10 minimal number of reads overlapping peak') + 
+#   ylab('number of peaks left')
+# # get shared peaks across cell types
+# sum_peaks_shared <- get_peaks_per_identity(sum_peaks$peaks)
+# plot_peak_sharing_per_celltype(sum_peaks_shared, use_label_dict = T, use_color_dict = T)
+# sum_peaks_shared_10 <- get_peaks_per_identity(sum_peaks$peaks, threshold = 1)
+# plot_peak_sharing_per_celltype(sum_peaks_shared_10, use_label_dict = T, use_color_dict = T)
+# 
+# # we'll do this per cell type instead of with FindAllMarkers, so we can have results in the mean time
+# da_peak_per_ct <- list()
+# Idents(mo_all) <- 'cell_type_final_lowerres'
+# for (cell_type in unique(mo_all@meta.data[['cell_type_final_lowerres']])) {
+#   print(cell_type)
+#   da_peaks <- FindMarkers(
+#     object = mo_all,
+#     ident.1 = cell_type,
+#     test.use = 'LR',
+#     latent.vars = 'nCount_peaks'
+#   )
+#   da_peak_per_ct[[cell_type]] <- da_peaks
+# }
