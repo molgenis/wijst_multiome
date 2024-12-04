c5bb02a1b81ec2263fbe930bc2eeb45e554aefbc
Roy Oelen <roy.oelen@gmail.com>
2024-12-04 16:11:18 +0100

    - updated the RDS extention to be uppercase for wg2 step3

--- ctc_gwas/mo_ctc_gwas_wg2_step3.sh
+++ ctc_gwas/mo_ctc_gwas_wg2_step3.sh
@@ -14,6 +14,6 @@ mkdir step3_hierscpred
 # check each rds file
 for i in $(ls step2_azimuth);
 do
-  out=$(echo $i | awk 'gsub(".rds", "")') # Use same base filename as output
+  out=$(echo $i | awk 'gsub(".RDS", "")') # Use same base filename as output
   singularity run -B /groups/umcg-franke-scrna/tmp01/ /groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/ctc_gwas/wg2/cell_classification.sif Rscript /groups/umcg-franke-scrna/tmp01/projects/sc-eqtlgen-consortium-pipeline/Depracated/wg2-cell-type-classification/wg2_wijst2020/map_hierscpred.R --file /groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/ctc_gwas/wg2/step2_azimuth/${i} --path /groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/ctc_gwas/wg2/step3_hierscpred --out ${out} --batch lane
 done
