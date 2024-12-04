9a697e260eec00d19f55b510686ed7011783a074
Roy Oelen <roy.oelen@gmail.com>
2024-12-04 15:55:06 +0100

    - use location of workaround script for WG2 step3

--- ctc_gwas/mo_ctc_gwas_wg2_step3.sh
+++ ctc_gwas/mo_ctc_gwas_wg2_step3.sh
@@ -15,5 +15,5 @@ mkdir step3_hierscpred
 for i in $(ls step2_azimuth);
 do
   out=$(echo $i | awk 'gsub(".rds", "")') # Use same base filename as output
-  singularity run -B /groups/umcg-franke-scrna/tmp01/ /groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/ctc_gwas/wg2/cell_classification.sif Rscript /map_hierscpred.R --file /groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/ctc_gwas/wg2/step2_azimuth/${i} --path /groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/ctc_gwas/wg2/step3_hierscpred --out ${out} --batch lane
+  singularity run -B /groups/umcg-franke-scrna/tmp01/ /groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/ctc_gwas/wg2/cell_classification.sif Rscript /groups/umcg-franke-scrna/tmp01/projects/sc-eqtlgen-consortium-pipeline/Depracated/wg2-cell-type-classification/wg2_wijst2020/map_hierscpred.R --file /groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/ctc_gwas/wg2/step2_azimuth/${i} --path /groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/ctc_gwas/wg2/step3_hierscpred --out ${out} --batch lane
 done
