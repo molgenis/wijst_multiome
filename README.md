c1c85d0eb304322fc9da89e5dede24b50038b492
Roy Oelen <roy.oelen@gmail.com>
2025-01-13 15:16:21 +0100

    - update readme - new mediation jobs created

--- README.md
+++ README.md
@@ -188,6 +188,7 @@ Below we will outline the steps taken to process and analyse the data
 
 
 ### QTL mediation
+'*qtl/mediation/mo_create_mediation_confinements.R*'    create confinement files for eQTL-by-caQTL mediation analyses\
 '*qtl/mediation/mo_create_mediation_metadata.R*'    create metadata for eQTL-by-caQTL mediation analyses\
 '*qtl/mediation/mo_perform_qtl_mediation_analysis.R*'    run eQTL-by-caQTL mediaton analysis script\
 '*qtl/mediation/mo_create_mediation_jobs.sh*'    create run eQTL-by-caQTL mediaton analysis jobs
--- qtl/mediation/mo_create_mediation_jobs.sh
+++ qtl/mediation/mo_create_mediation_jobs.sh
@@ -40,12 +40,12 @@ METADATA_PREPEND='/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/qtl/
 METADATA_APPEND='.metadata.tsv.gz'
 # output file prepend before cell type
 #OUT_PREPEND='/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/qtl/mediation/output/atac_to_expression/UT/'
-OUT_PREPEND='/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/qtl/mediation/output/regressed_atac_to_gausnormed_expression/UT/'
+OUT_PREPEND='/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/qtl/mediation/output/regressed_gausnormed_atac_to_gausnormed_expression/UT/'
 # output file append after cell type
 OUT_APPEND='.tsv.gz'
 # output file prepend before cell type
 #JOB_PREPEND='/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/qtl/mediation/jobs/atac_to_expression/UT/'
-JOB_PREPEND='/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/qtl/mediation/jobs/regressed_atac_to_gausnormed_expression/UT/'
+JOB_PREPEND='/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/qtl/mediation/jobs/regressed_gausnormed_atac_to_gausnormed_expression/UT/'
 # output file append after cell type
 JOB_APPEND='_SBATCH.sh'
 
@@ -103,7 +103,8 @@ do
         --random_effects '${RANDOM_EFFECTS}'
         --confinement_list '${confine_loc}'
         --out '${out_loc}'
-        --eqtl_gausnorm'
+        --eqtl_gausnorm
+        --caqtl_gausnorm'
 
         # add to the job
         echo ${run_command} >> ${job_loc}
