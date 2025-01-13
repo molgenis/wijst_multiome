63db6f91b346e25e77e505da8d6eb58020b25e70
Roy Oelen <roy.oelen@gmail.com>
2025-01-13 17:12:46 +0100

    - added md5sum creation

--- qtl/mo_regress_qtlinputs.py
+++ qtl/mo_regress_qtlinputs.py
@@ -33,6 +33,8 @@ from sklearn.linear_model import LinearRegression
 import concurrent.futures
 # argumenting
 import argparse
+# md5 checksum creation
+import hashlib
 
 
 ###############
@@ -47,6 +49,47 @@ def regress_out_pc(gene):
     residuals = y - model.predict(X)
     return gene, residuals
 
+# method to create md5
+def create_md5_file(input_file):
+    """
+    Creates an MD5 hash of the specified file and writes it to a new file with the same name but .md5 added to the extension.
+
+    Args:
+        input_file (str): The path to the input file for which the MD5 hash should be created.
+
+    Returns:
+        int: Returns 0 on success, 1 on failure.
+
+    Raises:
+        FileNotFoundError: Thrown if the input file does not exist.
+        IOError: Thrown if there is an error reading the input file (like permission denied) or writing the output md5 file.
+    """
+    try:
+        # get an md5 of the file
+        digest = None
+        with open(input_file, "rb") as f:
+            # try Python 3.11+ method if it is available
+            if callable(getattr(hashlib, 'file_digest', None)):
+                # digest with one command
+                digest = hashlib.file_digest(f, 'md5')
+            # or the older 3.8+ method if we don't have the newer method
+            else:
+                # initialize digest
+                digest = hashlib.md5()
+                # read file in chunks
+                while chunk := f.read(8192):
+                    # update digestion
+                    digest.update(chunk)       
+        # get the output path of the md5
+        output_md5_loc = ''.join([input_file, '.md5'])
+        # and write that
+        with open(output_md5_loc, "w") as m:
+            m.write(digest.hexdigest())
+        # upon success, return 0
+        return 0
+    except Exception as e:
+        print(f"Exception occured upon md5 file creation: {e}")
+        return 1
 
 ###############
 # optparse    #
@@ -106,3 +149,6 @@ if residuals_loc.endswith('.gz'):
     input_data_matched_residuals.to_csv(residuals_loc, sep = '\t', header = True, index = True, compression = 'gzip')
 else:
     input_data_matched_residuals.to_csv(residuals_loc, sep = '\t', header = True, index = True)
+
+# and make an md5
+create_md5_file(residuals_loc)
