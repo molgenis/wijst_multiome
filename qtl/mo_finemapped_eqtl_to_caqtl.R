cd8488d863f3686d812d7ea72245636a13ab79bb
Roy Oelen <roy.oelen@gmail.com>
2025-02-03 13:45:58 +0100

    - add function documentation

--- qtl/mo_finemapped_eqtl_to_caqtl.R
+++ qtl/mo_finemapped_eqtl_to_caqtl.R
@@ -2,7 +2,7 @@
 ############################################################################################################################
 # Authors: Roy Oelen
 # Name: mo_finemapped_eqtl_to_caqtl.R
-# Function: 
+# Function: check the overlap of effects for colocalizing eQTLs with caQTLs
 ############################################################################################################################
 
 ####################
@@ -12,11 +12,24 @@
 library(data.table)
 library(stringr)
 
+
 ####################
 # Settings        #
 ####################
 
-
+#' Extract Colocalizing Variants
+#'
+#' This function extracts colocalizing variants from a given dataset of finemapped colocalizations.
+#'
+#' @param finemapped_colocs A data frame containing finemapped colocalizations.
+#' @param cell_type_column A string specifying the column name for cell types. Default is 'ct'.
+#' @param feature_column A string specifying the column name for features (e.g., genes). Default is 'Gene'.
+#' @param variant_column A string specifying the column name for variants. Default is 'hit1'.
+#' @param PP_H4_abf_column A string specifying the column name for posterior probability (PP.H4.abf). Default is 'PP.H4.abf'.
+#' @param PP_H4_abf_column_cutoff A numeric value specifying the cutoff for posterior probability. Default is 0.8.
+#' @param link_split_char A string specifying the character used to split the feature link. Default is '_'.
+#'
+#' @return A data frame containing the colocalizing variants, genes, regions, and cell types.
 get_coloc_variants <- function(finemapped_colocs, cell_type_column='ct', feature_column='Gene', variant_column='hit1', PP_H4_abf_column='PP.H4.abf', PP_H4_abf_column_cutoff=0.8, link_split_char='_') {
   # subset to significant
   finemapped_colocs_colocing <- finemapped_colocs[finemapped_colocs[[PP_H4_abf_column]] >= PP_H4_abf_column_cutoff, ]
@@ -29,6 +42,31 @@ get_coloc_variants <- function(finemapped_colocs, cell_type_column='ct', feature
   return(finemapped_colocs_colocing_short)
 }
 
+
+#' Extract Beta Values for Cell Type
+#'
+#' This function extracts beta values for a specific cell type from a given QTL file and confinement table.
+#'
+#' @param cell_type_file_loc A string specifying the file location of the cell type data.
+#' @param confinement_table A data frame containing the confinement table.
+#' @param cell_type_name A string specifying the name of the cell type.
+#' @param confinement_variant_column A string specifying the column name for variants in the confinement table. Default is 'variant'.
+#' @param confinement_feature_column A string specifying the column name for features in the confinement table. Default is 'gene'.
+#' @param confinement_celltype_column A string specifying the column name for cell types in the confinement table. Default is 'cell_type'.
+#' @param qtl_variant_column A string specifying the column name for variants in the QTL file. Default is 'snp_id'.
+#' @param qtl_feature_column A string specifying the column name for features in the QTL file. Default is 'feature_id'.
+#' @param qtl_beta_column A string specifying the column name for beta values in the QTL file. Default is 'beta'.
+#' @param qtl_effect_column A string specifying the column name for the assessed allele in the QTL file. Default is 'assessed_allele'.
+#'
+#' @return A data frame containing the cell type, variants, features, beta values, and assessed alleles.
+#'
+#' @examples
+#' cell_type_file_loc <- "path/to/cell_type_file.txt"
+#' confinement_table <- data.frame(
+#'   cell_type = c('cell1', 'cell2'),
+#'   var_feature = c('variant1_gene1', 'variant2_gene2')
+#' )
+#' get_betas_file(cell_type_file_loc, confinement_table, 'cell1')
 get_betas_file <- function(cell_type_file_loc, confinement_table, cell_type_name, confinement_variant_column='variant', confinement_feature_column='gene', confinement_celltype_column='cell_type', qtl_variant_column='snp_id', qtl_feature_column='feature_id', qtl_beta_column='beta', qtl_effect_column='assessed_allele') {
   # read the file
   cell_type_table <- fread(cell_type_file_loc, header = T, sep = '\t')
@@ -44,6 +82,32 @@ get_betas_file <- function(cell_type_file_loc, confinement_table, cell_type_name
 }
 
 
+#' Extract Beta Values for Multiple Cell Types
+#'
+#' This function extracts beta values for multiple cell types from QTL files and a confinement table.
+#'
+#' @param qtl_output_loc A string specifying the directory location of the QTL output files.
+#' @param confinement_table A data frame containing the confinement table.
+#' @param cell_types A vector of strings specifying the cell types to include. Default is NULL, which includes all cell types.
+#' @param confinement_variant_column A string specifying the column name for variants in the confinement table. Default is 'variant'.
+#' @param confinement_feature_column A string specifying the column name for features in the confinement table. Default is 'gene'.
+#' @param confinement_celltype_column A string specifying the column name for cell types in the confinement table. Default is 'cell_type'.
+#' @param qtl_file_name A string specifying the name of the QTL file. Default is 'qtl_results_all.txt.gz'.
+#' @param qtl_variant_column A string specifying the column name for variants in the QTL file. Default is 'snp_id'.
+#' @param qtl_feature_column A string specifying the column name for features in the QTL file. Default is 'feature_id'.
+#' @param qtl_beta_column A string specifying the column name for beta values in the QTL file. Default is 'beta'.
+#' @param qtl_effect_column A string specifying the column name for the assessed allele in the QTL file. Default is 'assessed_allele'.
+#'
+#' @return A data frame containing the cell types, variants, features, beta values, and assessed alleles.
+#'
+#' @examples
+#' qtl_output_loc <- "path/to/qtl_output"
+#' confinement_table <- data.frame(
+#'   cell_type = c('cell1', 'cell2'),
+#'   variant = c('variant1', 'variant2'),
+#'   gene = c('gene1', 'gene2')
+#' )
+#' get_betas_per_celltype(qtl_output_loc, confinement_table)
 get_betas_per_celltype <- function(qtl_output_loc, confinement_table, cell_types=NULL, confinement_variant_column='variant', confinement_feature_column='gene', confinement_celltype_column='cell_type', qtl_file_name='qtl_results_all.txt.gz', qtl_variant_column='snp_id', qtl_feature_column='feature_id', qtl_beta_column='beta', qtl_effect_column='assessed_allele') {
   # get the var-gene combinations in the confinement
   confinement_table[['var_feature']] <- paste(confinement_table[[confinement_variant_column]], confinement_table[[confinement_feature_column]], sep = '_')
@@ -79,6 +143,52 @@ get_betas_per_celltype <- function(qtl_output_loc, confinement_table, cell_types
 }
 
 
+#' Merge Beta Values from Two Files
+#'
+#' This function merges beta values from two different files based on matching variants, features, and cell types.
+#'
+#' @param matching_table A data frame containing the matching table.
+#' @param beta_table1 A data frame containing the first set of beta values.
+#' @param beta_table2 A data frame containing the second set of beta values.
+#' @param matching_variant_column A string specifying the column name for variants in the matching table. Default is 'variant'.
+#' @param matching_feature1_column A string specifying the column name for the first feature in the matching table. Default is 'region'.
+#' @param matching_feature2_column A string specifying the column name for the second feature in the matching table. Default is 'gene'.
+#' @param matching_celltype_column A string specifying the column name for cell types in the matching table. Default is 'cell_type'.
+#' @param variant_column1 A string specifying the column name for variants in the first beta table. Default is 'variant'.
+#' @param variant_column2 A string specifying the column name for variants in the second beta table. Default is 'variant'.
+#' @param feature_column1 A string specifying the column name for features in the first beta table. Default is 'feature'.
+#' @param feature_column2 A string specifying the column name for features in the second beta table. Default is 'feature'.
+#' @param cell_type_column1 A string specifying the column name for cell types in the first beta table. Default is 'cell_type'.
+#' @param cell_type_column2 A string specifying the column name for cell types in the second beta table. Default is 'cell_type'.
+#' @param beta_column1 A string specifying the column name for beta values in the first beta table. Default is 'effect'.
+#' @param beta_column2 A string specifying the column name for beta values in the second beta table. Default is 'effect'.
+#' @param allele_column1 A string specifying the column name for alleles in the first beta table. Default is 'allele'.
+#' @param allele_column2 A string specifying the column name for alleles in the second beta table. Default is 'allele'.
+#'
+#' @return A data frame containing the merged beta values, along with the matching variants, features, and cell types.
+#'
+#' @examples
+#' matching_table <- data.frame(
+#'   cell_type = c('cell1', 'cell2'),
+#'   variant = c('variant1', 'variant2'),
+#'   region = c('region1', 'region2'),
+#'   gene = c('gene1', 'gene2')
+#' )
+#' beta_table1 <- data.frame(
+#'   cell_type = c('cell1', 'cell2'),
+#'   variant = c('variant1', 'variant2'),
+#'   feature = c('region1', 'region2'),
+#'   effect = c(0.5, 0.6),
+#'   allele = c('A', 'T')
+#' )
+#' beta_table2 <- data.frame(
+#'   cell_type = c('cell1', 'cell2'),
+#'   variant = c('variant1', 'variant2'),
+#'   feature = c('gene1', 'gene2'),
+#'   effect = c(0.4, 0.7),
+#'   allele = c('A', 'C')
+#' )
+#' merge_beta_files(matching_table, beta_table1, beta_table2)
 merge_beta_files <- function(
     matching_table,
     beta_table1, 
@@ -145,14 +255,14 @@ merge_beta_files <- function(
 ####################
 
 # location of the finemapped results
-finemapped_colocs_loc <- '/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/qtl/caqtl/sc-eqtlgen/multiome_eqtl/finemapping/coloc_e_iQTL_vs_caQTL/complete_results_all_cts.txt'
+finemapped_colocs_loc <- '/groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/qtl/caqtl/sc-eqtlgen/multiome_eqtl/finemapping/coloc_e_iQTL_vs_caQTL/complete_results_all_cts.txt'
 # read the tabel
 finemapped_colocs <- fread(finemapped_colocs_loc, header = T, sep = '\t')
 # location of the eQTL output
-eqtl_output_loc <- '/groups/umcg-franke-scrna/tmp04/projects/sc-eqtlgen-consortium-pipeline/ongoing/wg3/wg3_multiome/output/L1/UT/'
+eqtl_output_loc <- '/groups/umcg-franke-scrna/tmp02/projects/sc-eqtlgen-consortium-pipeline/ongoing/wg3/wg3_multiome/output/L1/UT/'
 eqtl_file_name <- 'qtl_results_all_nominally_significant.txt.gz'
 # location of the caQTL output
-caqtl_output_loc <- '/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/qtl/caqtl/sc-eqtlgen/combined_output_50kb/FDR_all_effects/'
+caqtl_output_loc <- '/groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/qtl/caqtl/sc-eqtlgen/combined_output_50kb/FDR_all_effects/'
 # get the colocs
 significant_finemapped_colocs <- get_coloc_variants(finemapped_colocs)
 # get the eqtl betas
@@ -184,7 +294,7 @@ caqtl_betas_unique <- unique(data.frame(caqtl_betas))
 # # add cell type to region to variant
 # finemapped_colocs_varfeature[['var_feat_ct']] <- paste(finemapped_colocs_varfeature[['variant']], finemapped_colocs_varfeature[['region']], finemapped_colocs_varfeature[['cell_type']])
 # # get the full stats
-# all_ca_loc <- '/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/qtl/caqtl/sc-eqtlgen/combined_output_50kb/FDR_all_effects/ALL_UT__conditions_speific_0001.tsv'
+# all_ca_loc <- '/groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/qtl/caqtl/sc-eqtlgen/combined_output_50kb/FDR_all_effects/ALL_UT__conditions_speific_0001.tsv'
 # all_ca <- fread(all_ca_loc, header = T, sep = '\t')
 # # add the same in the caQTL table
 # all_ca[['var_feat_ct']] <- paste(all_ca[['snp_id']], all_ca[['feature_id']], all_ca[['ct']])
@@ -212,10 +322,10 @@ for (cell_type in unique(betas_both[['cell_type']])) {
   print(paste('con:', sum(sign(betas_both_ct$effect1)  == sign(betas_both_ct$effect2)) / nrow(betas_both_ct)))
   print(paste('cor:', cor(betas_both_ct$effect1, betas_both_ct$effect2)))
 }
-write.table(betas_both, gzfile('/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/qtl/mediation/coloc/mo_colocing_betas.tsv.gz'), row.names = F, col.names = T, sep = '\t', quote = F)
+write.table(betas_both, gzfile('/groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/qtl/mediation/coloc/mo_colocing_betas.tsv.gz'), row.names = F, col.names = T, sep = '\t', quote = F)
 
 # read the file again
-betas_both <- read.table('/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/qtl/mediation/coloc/mo_colocing_betas.tsv.gz', header = T, sep = '\t')
+betas_both <- read.table('/groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/qtl/mediation/coloc/mo_colocing_betas.tsv.gz', header = T, sep = '\t')
 
 # get the min and max values on the axis
 max_beta1 <- max(abs(betas_both[['effect1']]))
