5822dbf6ddeb0f96f6bc5cf1bbb383cf5684e3a5
Roy Oelen <roy.oelen@gmail.com>
2025-02-18 15:24:15 +0100

    - aggregate for permutations as well

--- qtl/cre_qtl/mo_aggregated_creqtl_inputs.sh
+++ qtl/cre_qtl/mo_aggregated_creqtl_inputs.sh
@@ -2,26 +2,32 @@
 ###################################################################
 #Script Name	  : mo_aggregated_creqtl_inputs.sh
 #Description	  : aggregate all the CRE QTL inputs
-#Args           : cre location, folder containing per-sample p/beta/se, folder to output aggregated matrices
+#Args           : cre location, folder containing per-sample p/beta/se, folder to output aggregated matrices, optional number of permutations to check for
 #Author       	: Roy Oelen
 #example        : /groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/qtl/cre_eqtl/pseudobulk_replication/scripts/mo_aggregated_creqtl_inputs.sh \
 #                 /groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/qtl/coeqtl/trial_run/cre_lists/mono_cre_scenic_and_pseudo.tsv.gz \
-#                 /groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/qtl/cre_eqtl/pseudobulk_replication/betas_ps/monocyte/ \
-#                 /groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/qtl/cre_eqtl/pseudobulk_replication/betas_ps/monocyte/
-#                 
+#                 /groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/qtl/cre_eqtl/pseudobulk_replication/betas_ps_wperm/monocyte/ \
+#                 /groups/umcg-franke-scrna/tmp02/projects/multiome/ongoing/qtl/cre_eqtl/pseudobulk_replication/betas_ps_wperm/monocyte/ \
+#                 10
 ###################################################################
 
 # the parameters supplied
 CRE_LOC=$1
 SUMMARIES_LOC=$2
 OUTPUT_LOC=$3
+N_PERM=$4
+# default permutations is 0
+if [ -z "${4}" ];
+    then
+    N_PERM='0'
+fi
 
 # generate random UUID for the temporary file
 uuid=$(uuidgen)
 # generate temporary beta/p/se files
-tmp_ps=${OUTPUT_LOC}'/'${uuid}'.betas.tsv'
+tmp_ps=${OUTPUT_LOC}'/'${uuid}'.ps.tsv'
 tmp_betas=${OUTPUT_LOC}'/'${uuid}'.betas.tsv'
-tmp_ses=${OUTPUT_LOC}'/'${uuid}'.betas.tsv'
+tmp_ses=${OUTPUT_LOC}'/'${uuid}'.ses.tsv'
 # and full files
 ps_full=${OUTPUT_LOC}'/ps.tsv'
 betas_full=${OUTPUT_LOC}'/betas.tsv'
@@ -41,6 +47,22 @@ zcat ${CRE_LOC} > ${ps_full}
 zcat ${CRE_LOC} > ${betas_full}
 zcat ${CRE_LOC} > ${ses_full}
 
+# make a list for the permutations
+perm_numbers=[]
+# to be filled if there were permutations
+if [ "$N_PERM" != "0" ]; then
+    # permutations were zero-based, so we need to stop one earlier
+    perm_stop="$((N_PERM-1))"
+    # use seq to put them in the array
+    perm_numbers=($(seq 0 1 ${perm_stop}))
+fi
+# initialize all the permutation files
+for perm in ${perm_numbers[@]}; do
+    zcat ${CRE_LOC} > ${OUTPUT_LOC}'/perm_'${perm}'_betas.tsv'
+    zcat ${CRE_LOC} > ${OUTPUT_LOC}'/perm_'${perm}'_ses.tsv'
+    zcat ${CRE_LOC} > ${OUTPUT_LOC}'/perm_'${perm}'_ps.tsv'
+    echo -en "region\tgene" > ${OUTPUT_LOC}'perm_'${perm}'_starting_columns.tsv'
+done
 
 # check each directory
 for sum_dir in ${dirlist[@]}; do
@@ -87,6 +109,57 @@ for sum_dir in ${dirlist[@]}; do
             rm ${betas_full}
             mv ${tmp_betas} ${betas_full}
             rm ${betas_file_extracted}
+
+            # now try to do the same for the permutations
+            for perm in ${perm_numbers[@]}; do
+                # paste the full path to the betas file
+                perm_betas_file=${sum_dir}'/perm_'${perm}'_beta.txt.gz'
+                # next, check if it has output, by searching for the betas file
+                if [ -e "$perm_betas_file" ]; then
+                    # add to the header if we have this file
+                    echo -en '\t'${sample_name} >> ${OUTPUT_LOC}'/perm_'${perm}'_starting_columns.tsv'
+                    
+                    # do the same for the betas and ps
+                    perm_ps_file=${sum_dir}'/perm_'${perm}'_p.txt.gz'
+                    perm_ps_file_extracted=${sum_dir}'/perm_'${perm}'_p.txt'
+                    # unzip file
+                    gunzip -c ${perm_ps_file} > ${perm_ps_file_extracted}
+                    # generate temporary beta/p/se files
+                    perm_tmp_ps=${OUTPUT_LOC}'/'${uuid}'.perm_'${perm}'.ps.tsv'
+                    perm_tmp_betas=${OUTPUT_LOC}'/'${uuid}'.perm_'${perm}'.betas.tsv'
+                    perm_tmp_ses=${OUTPUT_LOC}'/'${uuid}'.perm_'${perm}'.ses.tsv'
+                    # and the full files
+                    perm_ps_full=${OUTPUT_LOC}'/perm_'${perm}'_ps.tsv'
+                    perm_betas_full=${OUTPUT_LOC}'/perm_'${perm}'_betas.tsv'
+                    perm_ses_full=${OUTPUT_LOC}'/perm_'${perm}'_ses.tsv'
+                    
+                    # paste into temporary file
+                    paste ${perm_ps_full} ${perm_ps_file_extracted} > ${perm_tmp_ps}
+                    # remove old full file
+                    rm ${perm_ps_full}
+                    # make tmp file new file
+                    mv ${perm_tmp_ps} ${perm_ps_full}
+                    # remove unzipped file
+                    rm ${perm_ps_file_extracted}
+
+                    # do the same for ses
+                    perm_ses_file=${sum_dir}'/perm_'${perm}'_se.txt.gz'
+                    perm_ses_file_extracted=${sum_dir}'/perm_'${perm}'_se.txt'
+                    gunzip -c ${perm_ses_file} > ${perm_ses_file_extracted}
+                    paste ${perm_ses_full} ${perm_ses_file_extracted} > ${perm_tmp_ses}
+                    rm ${perm_ses_full}
+                    mv ${perm_tmp_ses} ${perm_ses_full}
+                    rm ${perm_ses_file_extracted}
+
+                    # and beta finally
+                    perm_betas_file_extracted=${sum_dir}'/perm_'${perm}'_beta.txt'
+                    gunzip -c ${perm_betas_file} > ${perm_betas_file_extracted}
+                    paste ${perm_betas_full} ${perm_betas_file_extracted} > ${perm_tmp_betas}
+                    rm ${perm_betas_full}
+                    mv ${perm_tmp_betas} ${perm_betas_full}
+                    rm ${perm_betas_file_extracted}
+                fi
+            done
         fi
     fi
 done
@@ -119,3 +192,49 @@ mv ${tmp_ses} ${ses_full}
 bgzip ${ps_full}
 bgzip ${betas_full}
 bgzip ${ses_full}
+
+# and clean up
+rm ${aggregated_columns_loc}
+
+# permutations as well
+for perm in ${perm_numbers[@]}; do
+# generate temporary beta/p/se files    
+    perm_tmp_ps=${OUTPUT_LOC}'/'${uuid}'.perm_'${perm}'.ps.tsv'
+    perm_tmp_betas=${OUTPUT_LOC}'/'${uuid}'.perm_'${perm}'.betas.tsv'
+    perm_tmp_ses=${OUTPUT_LOC}'/'${uuid}'.perm_'${perm}'.ses.tsv'
+    # and the full files
+    perm_ps_full=${OUTPUT_LOC}'/perm_'${perm}'_ps.tsv'
+    perm_betas_full=${OUTPUT_LOC}'/perm_'${perm}'_betas.tsv'
+    perm_ses_full=${OUTPUT_LOC}'/perm_'${perm}'_ses.tsv'
+    # add newline to the headers
+    echo -en '\n' >> ${OUTPUT_LOC}'perm_'${perm}'_starting_columns.tsv'
+    # also add the headers now
+    cat ${OUTPUT_LOC}'perm_'${perm}'_starting_columns.tsv' > ${perm_tmp_ps}
+    # and the content
+    cat ${perm_ps_full} >> ${perm_tmp_ps}
+    # remove old full file
+    rm ${perm_ps_full}
+    # and make the temporary file the new one
+    mv ${perm_tmp_ps} ${perm_ps_full}
+
+    # same for the betas
+    cat ${OUTPUT_LOC}'perm_'${perm}'_starting_columns.tsv' > ${perm_tmp_betas}
+    cat ${perm_betas_full} >> ${perm_tmp_betas}
+    rm ${perm_betas_full}
+    mv ${perm_tmp_betas} ${perm_betas_full}
+
+    # and the ses
+    cat ${OUTPUT_LOC}'perm_'${perm}'_starting_columns.tsv' > ${perm_tmp_ses}
+    cat ${perm_ses_full} >> ${perm_tmp_ses}
+    rm ${perm_ses_full}
+    mv ${perm_tmp_ses} ${perm_ses_full}
+
+    # finally zip them all
+    bgzip ${perm_ps_full}
+    bgzip ${perm_betas_full}
+    bgzip ${perm_ses_full}
+
+    # and clean
+    rm ${OUTPUT_LOC}'perm_'${perm}'_starting_columns.tsv'
+
+done
