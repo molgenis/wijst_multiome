224e1b8b714e1d880d98e546c92bbeb662d2b528
Roy Oelen <r.oelen@umcg.nl>
2025-05-20 21:24:55 +0200

    - convert coloc rds to tsv

--- qtl/colocalization/mo_coloc_qtls.R
+++ qtl/colocalization/mo_coloc_qtls.R
@@ -170,6 +170,125 @@ coloc_datasets <- function(dataset1_fm_table,
 }
 
 
+#' Convert SuSiE RSS Output to Table
+#'
+#' This function takes unformatted SuSiE RSS output and converts it into a formatted table.
+#'
+#' @param unformatted_susie_output A list of unformatted SuSiE RSS output, where each element contains a feature and its corresponding result.
+#' @param feature_key A list character string denoting the key used for the feature
+#' @return A data frame containing the formatted results for each feature, including feature name, variant, lambda, PIP, residual variance estimation, credible sets, and log Bayes factors.
+#' @examples
+#' # Example usage:
+#' unformatted_output <- list(
+#'   list(feature = "feature1", result = list(susie_rss = list(converged = TRUE, alpha = matrix(0.1, ncol = 3), pip = c(0.2, 0.3, 0.5), sets = list(cs = list(1:2)), lbf_variable = matrix(0.4, ncol = 3)), lambda = 0.5)),
+#'   list(feature = "feature2", result = list(susie_rss = list(converged = FALSE), lambda = 0.7))
+#' )
+#' formatted_table <- susie_rss_to_table(unformatted_output)
+#' print(formatted_table)
+susie_rss_to_table <- function(unformatted_susie_output, feature_key='feature') {
+  # create a list of each feature
+  formatted_fm_per_feature <- list()
+  # and keep track of the max number of sets
+  max_sets <- 0
+  # check each of the outputs
+  for (i in 1 : length(unformatted_susie_output)) {
+    # extract the output
+    unformatted_susie_output_feature <- unformatted_susie_output[[i]]
+    # get the feature
+    feature_output <- unformatted_susie_output_feature[[feature_key]]
+    # and the result
+    feature_result <- unformatted_susie_output_feature[['result']]
+    # extract the susie_rss result
+    feature_result_susierss <- feature_result[['susie_rss']]
+    # check if converged
+    if (feature_result_susierss$converged) {
+      # create a table with the ensemble ID and the variants
+      feature_result_res_table <- data.frame('feature' = rep(feature_output, times = ncol(feature_result_susierss$alpha)), 'variant' = colnames(feature_result_susierss$alpha))
+      # add the lambda
+      feature_result_res_table[['lambda']] <- feature_result[['lambda']]
+      # add the pip
+      feature_result_res_table[['pip']] <- feature_result_susierss[['pip']][match(feature_result_res_table[['variant']], names(feature_result_susierss[['pip']]))]
+      # add whether we estimated the residual variance
+      if ('resvar' %in% names(feature_result)) {
+        feature_result_res_table[['resvar']] <- feature_result_susierss[['resvar']]
+      }
+      else {
+        feature_result_res_table[['resvar']] <- NA
+      }
+      # and if it converged
+      feature_result_res_table[['converged']] <- T
+      # get the number of iterations we used
+      feature_result_res_table[['n_iter']] <- feature_result_susierss[['niter']]
+      # initialize the credible sets
+      feature_result_res_table[['CS']] <- NA
+      # check if we have credible sets
+      if(length(feature_result_susierss[['sets']][[1]])!=0){
+        # check each set
+        for(l in names(feature_result_susierss[['sets']][['cs']])){
+          # set for those positions, which set it was
+          indices_variant_set <- feature_result_susierss[['sets']][['cs']][[l]]
+          # and set for those
+          feature_result_res_table[indices_variant_set, 'CS'] <- l
+        }
+      }
+      # get the lbf, and transpose
+      lbf_out <- data.frame(t(feature_result_susierss[['lbf_variable']]))
+      # replace the X in the name, with CS
+      colnames(lbf_out) <- paste0('CS', 1:ncol(lbf_out))
+      # add in the correct order
+      if (nrow(lbf_out) > 1) {
+        feature_result_res_table <- cbind(feature_result_res_table, lbf_out[match(feature_result_res_table[['variant']], rownames(lbf_out)), , drop = F])
+      }
+      # except when there is only one variant, then this would mess up the column names
+      else {
+        feature_result_res_table <- cbind(feature_result_res_table, lbf_out)
+      }
+      # and put into the list
+      formatted_fm_per_feature[[feature_output]] <- feature_result_res_table
+      # update the max number of sets if this was higher
+      if (ncol(lbf_out) > max_sets) {
+        max_sets <- ncol(lbf_out)
+      }
+    }
+    else {
+      feature_result_res_table <- data.frame(
+        'feature' = c(feature_output), 
+        'variant' = c(NA), 
+        'lambda' = c(NA), 
+        'pip' = c(NA), 
+        'resvar' = c(NA), 
+        'converged' = c(F), 
+        'n_iter' = c(feature_result_susierss[['niter']]), 
+        'CS' = c(NA)
+      )
+      # and put into the list
+      formatted_fm_per_feature[[feature_output]] <- feature_result_res_table
+    }
+  }
+  # these are the columns that are always present
+  permanent_columns <- c('feature', 'variant', 'lambda', 'pip', 'resvar', 'converged', 'n_iter', 'CS')
+  # check each per-feature output
+  for(feature_output in names(formatted_fm_per_feature)) {
+    # and check if the number of columns is less than we would expect based on the max number of sets
+    if (ncol(formatted_fm_per_feature[[feature_output]]) < (length(permanent_columns) + max_sets)) {
+      # then get get which columns are missing
+      missing_columns <- setdiff(paste0('CS', 1 : max_sets), colnames(formatted_fm_per_feature[[feature_output]]))
+      # we need to make the number of column the same across the tables, or the rbind will fail. S we create an empty data frame
+      missing_data <- data.frame(matrix(NA, nrow = nrow(formatted_fm_per_feature[[feature_output]]), ncol = length(missing_columns)))
+      # with the correct row names
+      rownames(missing_data) <- rownames(formatted_fm_per_feature[[feature_output]])
+      # and the columns that we are missing from the total
+      colnames(missing_data) <- missing_columns
+      # and merge these missing columns onto the existing data
+      formatted_fm_per_feature[[feature_output]] <- cbind(formatted_fm_per_feature[[feature_output]], missing_data)
+    }
+  }
+  # merge all of them together
+  formatted_fm_all <- do.call('rbind', formatted_fm_per_feature)
+  return(formatted_fm_all)
+}
+
+
 ####################
 # Settings        #
 ####################
@@ -209,6 +328,7 @@ caqtl_finemapping_append <- '_finemapped.tsv.gz'
 # prepend of coloc
 coloc_prepend <- ''
 coloc_append <- '_eqtl_caqtl_coloc.rds'
+coloc_tbl_append <- '_eqtl_caqtl_coloc.tsv.gz'
 
 # name of the first data
 dataset1_name <- 'caQTLs'
@@ -231,6 +351,8 @@ for (cell_type in cell_types) {
   caqtls <- fread(caqtl_path, header = T, sep = '\t')
   # store result per chromosome
   fm_per_chrom <- list()
+  # also with tables
+  fm_tbl_per_chrom <- list()
   # check each of the chromosomes
   # for (chrom in unique(region_gene_distances[['chromosome']])) {
   for (chrom in unique(gene_annotation[['chromosome']])) {
@@ -254,6 +376,8 @@ for (cell_type in cell_types) {
     )
     # put in the list
     fm_per_chrom[[as.character(chrom)]] <- coloc_chrom
+    # convert to a table
+    fm_tbl_per_chrom[[as.character(chrom)]] <- susie_rss_to_table(coloc_chrom)
   }
   # paste together the output location
   coloc_ct_output_loc <- paste0(coloc_output_loc, '/', coloc_prepend, cell_type, coloc_append)
@@ -261,4 +385,10 @@ for (cell_type in cell_types) {
   saveRDS(fm_per_chrom, coloc_ct_output_loc)
   # make a checksum
   mdfiver::create_md5_for_file(coloc_ct_output_loc)
+  # merge the tables
+  fm_tbl_all_chrom <- do.call('rbind', fm_tbl_per_chrom)
+  # write the table
+  coloc_tbl_ct_output_loc <- paste0(coloc_output_loc, '/', coloc_prepend, cell_type, coloc_tbl_append)
+  write.table(fm_tbl_all_chrom, gzfile(coloc_tbl_ct_output_loc), row.names = F, col.names = T, sep = '\t')
+  mdfiver::create_md5_for_file(coloc_tbl_ct_output_loc)
 }
