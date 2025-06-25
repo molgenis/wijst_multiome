2af6e91b4c3804070450d94335b50d6bc6678da0
Roy Oelen <roy.oelen@gmail.com>
2025-06-25 16:26:12 +0200

    - save overlap file also separately for cis-only vs trans-acting eQTLs of eQTLgen

--- cisonly_vs_transacting/mo_create_creqtl_confinement_cistrans_eqtlgen.R
+++ cisonly_vs_transacting/mo_create_creqtl_confinement_cistrans_eqtlgen.R
@@ -108,3 +108,63 @@ overlap_r2g <- unique(overlap_all[, c('region', 'gene')])
 write.table(overlap_r2g, gzfile(confinement_r2g_loc), sep = '\t', row.names = F, col.names = F, quote = F)
 # with a checksum again
 mdfiver::create_md5_for_file(confinement_r2g_loc)
+
+
+# load the original input from eQTLgen
+eqtlgen_pairs_loc <- '/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/eqtlgen_replications/cisonly_vs_transacting/qtl_tables/independent_variants_filtered_lbf2_mlog10p5_annotated_20250509_filtered-maxR2_0.9-noHla-noCrossmapping.txt'
+# read this table
+eqtlgen_pairs <- fread(eqtlgen_pairs_loc, header = T, sep = '\t')
+# make the variant table
+variants_unique <- unique(eqtlgen_pairs[['variant']])
+variant_annotation_table <- data.table('variant' = variants_unique, 'in_open' = rep(NA, times = length(variants_unique)), 'overlaps_caqtl' = rep(NA, times = length(variants_unique)))
+# in the right type
+variant_annotation_table[['in_open']] <- as.character(variant_annotation_table[['in_open']])
+variant_annotation_table[['overlaps_caqtl']] <- as.character(variant_annotation_table[['overlaps_caqtl']])
+
+# show how many variants we have
+message(paste('checking', as.character(nrow(variant_annotation_table)), 'variants'))
+
+# check each variant
+for (i in 1 : nrow(variant_annotation_table)) {
+  # extract variant
+  variant_to_check <- variants_unique[i]
+  # get the rows for that variant
+  variants_confinement <- overlap_all[!is.na(overlap_all[['variant']]) & overlap_all[['variant']] == variant_to_check, ]
+  # setup variables for which cell types have openness or caQTLs
+  openness_cts_string <- NA
+  caqtl_cts_string <- NA
+  # check if there were any matches
+  if (nrow(variants_confinement) > 0) {
+    # subset to the openness inclusion
+    variants_confinement_openness <- variants_confinement[!is.na(variants_confinement[['inclusion']]) & variants_confinement[['inclusion']] == 'openness', ]
+    # if there are any, update the value
+    if (nrow(variants_confinement_openness) > 0) {
+      openness_cts <- unique(variants_confinement_openness[['cell_type']])
+      openness_cts_string <- paste(openness_cts, collapse = ';')
+    }
+    # do the same for the caQTLs
+    variants_confinement_caqtls <- variants_confinement[!is.na(variants_confinement[['inclusion']]) & variants_confinement[['inclusion']] == 'caqtl', ]
+    # if there are any, update the value
+    if (nrow(variants_confinement_caqtls) > 0) {
+      caqtl_cts <- unique(variants_confinement_caqtls[['cell_type']])
+      caqtl_cts_string <- paste(caqtl_cts, collapse = ';')
+    }
+  }
+  # update the table
+  variant_annotation_table[i, 'variant'] <- variant_to_check
+  if (!is.na(openness_cts_string)) {
+    variant_annotation_table[i, 'in_open'] <- openness_cts_string
+  }
+  if (!is.na(caqtl_cts_string)) {
+    variant_annotation_table[i, 'overlaps_caqtl'] <- caqtl_cts_string
+  }
+  if (i %% 100 == 0) {
+    message(paste('processed', as.character(i), 'variants'))
+  }
+}
+# now merge them
+eqtlgen_merged <- merge(x = eqtlgen_pairs, y = variant_annotation_table, all.x = T, all.y = F, by = 'variant')
+# write result
+annotated_eqtlgen_pairs_loc <- '/groups/umcg-franke-scrna/tmp04/projects/multiome/ongoing/eqtlgen_replications/cisonly_vs_transacting/qtl_tables/independent_variants_filtered_lbf2_mlog10p5_annotated_20250509_filtered-maxR2_0.9-noHla-noCrossmapping_chromatinadded.txt.gz'
+write.table(eqtlgen_merged, gzfile(annotated_eqtlgen_pairs_loc), row.names = F, col.names = T, sep = '\t', quote = F)
+mdfiver::create_md5_for_file(annotated_eqtlgen_pairs_loc)
