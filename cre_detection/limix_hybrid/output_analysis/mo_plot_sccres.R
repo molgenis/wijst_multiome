6fb06d0d86d96b1b64572adc99e2d3b1f070362a
Roy Oelen <roy.oelen@gmail.com>
2026-02-16 19:31:12 +0100

    - plot concordance

--- cre_detection/limix_hybrid/output_analysis/mo_plot_sccres.R
+++ cre_detection/limix_hybrid/output_analysis/mo_plot_sccres.R
@@ -1340,6 +1340,61 @@ get_overlapping_peakgene_links <- function(cre_tbl1, cre_tbl2, region_column1='s
   return(results_all)
 }
 
+# plot the concordance
+plot_concordanace <- function(qtl_effect_table, ca_effect_column='ca_effect', e_effect_column='e_effect', main='Effect sizes of caQTLs versus eQTLs', xlab='caQTL effect size', ylab='eQTL effect size') {
+  # set standardized columns
+  qtl_effect_table[['ca_effect']] <- qtl_effect_table[[ca_effect_column]]
+  qtl_effect_table[['e_effect']] <- qtl_effect_table[[e_effect_column]]
+  
+  # get the concordance
+  con_qtl_effect_table <- sum(sign(qtl_effect_table[['e_effect']]) == sign(qtl_effect_table[['ca_effect']])) / nrow(qtl_effect_table)
+  
+  # get the min and max values on the axis
+  max_beta1 <- max(abs(qtl_effect_table[['ca_effect']]))
+  min_beta1 <- min(abs(qtl_effect_table[['ca_effect']]))
+  max_beta2 <- max(abs(qtl_effect_table[['e_effect']]))
+  min_beta2 <- min(abs(qtl_effect_table[['e_effect']]))
+  
+  # start making the plot
+  plot(x = qtl_effect_table[['ca_effect']], y = qtl_effect_table[['e_effect']], 
+       ylim = c(max_beta2 * -1, max_beta2),
+       xlim = c(max_beta1 * -1, max_beta1),
+       xlab = xlab,
+       ylab = ylab,
+       pch = 16,
+       cex = 0.5,
+       main = main
+  )
+  # the top to bottom rectangle of non-significant effects
+  rect(xleft = -1 * min_beta1, xright = min_beta1, ybottom = -1 * max_beta2, ytop = max_beta2, border = NA, col = rgb(red = 1, green = 1, blue = 1, alpha = 0.5))
+  # the left to right rectangle of non-significant effects
+  rect(xleft = -1 * max_beta1, xright = max_beta1, ybottom = -1 * min_beta2, ytop = min_beta2, border = NA, col = rgb(red = 1, green = 1, blue = 1, alpha = 0.5))
+  # bottom left concordant
+  rect(xleft = -1 * max_beta1, xright = -1 *min_beta1, ybottom = -1 * max_beta2, ytop = -1 * min_beta2, border = NA, col = rgb(red = 0, green = .45, blue = .7, alpha = 0.5))
+  # bottom right disconcordant
+  rect(xleft = min_beta1, xright = max_beta1, ybottom = -1 * max_beta2, ytop = -1 * min_beta2, border = NA, col = rgb(red = .8, green = .4, blue = 0, alpha = 0.5))
+  # top left disconcordant
+  rect(xleft = -1 * max_beta1, xright = -1 *min_beta1, ybottom = min_beta2, ytop = max_beta2, border = NA, col = rgb(red = .8, green = .4, blue = 0, alpha = 0.5))
+  # top right disconcordant
+  rect(xleft = min_beta1, xright = max_beta1, ybottom = min_beta2, ytop = max_beta2, border = NA, col = rgb(red = 0, green = .45, blue = .7, alpha = 0.5))
+  # box to put concordance label in
+  # rect(xleft = max_beta2 * 0.60, xright = max_beta2 * 0.95, ybottom = max_beta2 * -0.8, ytop = max_beta2 * -0.70, col = 'white')
+  rect(xleft = max_beta2 * 0.50, xright = max_beta2 * 1.15, ybottom = max_beta2 * -0.9, ytop = max_beta2 * -0.60, col = 'white')
+  # add concordance label
+  text(x = max_beta1 * 0.75 , y = max_beta2 * -0.75, labels = c(paste('concordance', round(con_qtl_effect_table, digits = 2), sep = ':\n')))
+  # add the annotation of what if concordant and disconcordant
+  #text(x = max_sig_z_venema * 0.75 , y = max_sig_z_eqtlgen * 0.75, labels = c('concordant'), col = rgb(red = 0, green = .45, blue = .7))
+  #text(x = max_sig_z_venema * - 0.75 , y = max_sig_z_eqtlgen * 0.75, labels = c('disconcordant'), col = rgb(red = .8, green = .4, blue = 0))
+  # add dashed lines
+  lines(c(max_beta1 * -1, max_beta1), c(min_beta2 * -1, min_beta2 * -1), type = "l", lty = 2)
+  lines(c(max_beta1 * -1, max_beta1), c(min_beta2, min_beta2), type = "l", lty = 2)
+  lines(c(min_beta1 * -1, min_beta1 * -1), c(max_beta2 * -1, max_beta2), type = "l", lty = 2)
+  lines(c(min_beta1, min_beta1), c(max_beta2 * -1, max_beta2), type = "l", lty = 2)
+  # save the plot
+  # plots/mo_top_eqtl_over_ct_to_caqtl_betas.pdf
+  # 5,5
+}
+
 
 ####################
 # Settings         #
@@ -1573,3 +1628,12 @@ p_overlapping_r2g_ps_sc <- ggplot(data = cre_overlapping, mapping = aes(x = cell
 p_overlapping_r2g_ps_sc
 # save the figure
 ggsave('~/plots/mo_sccre_vs_pscre_celltype_overlap.pdf', plot = p_overlapping_r2g_ps_sc, width = 4, height = 4)
+
+# sort by effect size
+hybrid_output <- hybrid_output[order(abs(hybrid_output[['zscore']])), ]
+pseudobulk_output <- pseudobulk_output[order(abs(pseudobulk_output[['zscore']])), ]
+# merge significant results
+overlap_top_effects <- merge(pseudobulk_output[!duplicated(paste(pseudobulk_output[['snp_id']], pseudobulk_output[['feature_id']])), ], hybrid_output[!duplicated(paste(hybrid_output[['snp_id']], hybrid_output[['feature_id']])), ], by = c('snp_id', 'feature_id'))
+# plot this
+plot_concordanace(overlap_top_effects, ca_effect_column='zscore.x', e_effect_column='zscore.y', main='Effect sizes of pbCREs vs scCREs', xlab = 'pbCRE Z-score', ylab = 'scCRE Z-score')
+
